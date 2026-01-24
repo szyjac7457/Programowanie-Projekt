c5dbe5f799ab23eb598c0939ae9495351b6d3da6
szyjac7457 <290403@student.pwr.edu.pl>
2026-01-24 14:59:50 +0100

    Funkcja sprawdzająca dostępność w danej godzinie

--- data_converter.py
+++ data_converter.py
@@ -20,3 +20,43 @@ def availability_grid(raw_data, field_name):
             day: {slot: False for slot in SLOTS}
             for day in range (1, 8)
         }
+
+    for item in raw_data:
+        id = item[field_name]
+        day = item['weekday']
+        start_min = time_to_min(item['time_start'])
+        end_min = time_to_min(item['time_end'])
+
+        for slot in SLOTS:
+            lesson_time = time_to_min(slot)
+            if start_min <= lesson_time and end_min >= (lesson_time + lesson_duration):
+                grid[id][day][slot] = True
+    
+    return grid
+
+def main():
+    test = [
+        {
+            "student_id": 1,
+            "weekday": 1,
+            "time_start": "14:00",
+            "time_end": "16:00"
+        }
+    ]
+
+    print("Sprawdzam dostępność dla ucznia ID = 1...")
+    print("-" * 40)
+
+    result = availability_grid(test, "student_id")
+    poniedzialek = result[1][1]
+
+    for slot, is_ok in poniedzialek.items():
+        if is_ok:
+            print(f"{slot} PASUJE")
+        else:
+            print(f"{slot}NIE PASUJE")
+
+
+if __name__ == "__main__":
+    main()
+
