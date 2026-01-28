58a9d526d46da20b6a71f1704d51644dbb44af3b
szyjac7457 <290403@student.pwr.edu.pl>
2026-01-28 18:47:19 +0100

    W pełni działający data_converter oraz poprawka przecinków w tutors_subject.csv

--- data_converter.py
+++ data_converter.py
@@ -1,3 +1,7 @@
+import os
+import json
+import csv
+
 SLOTS = ["12:00", "13:05", "14:10", "15:15", "16:35", "17:40", "18:45"]
 lesson_duration = 60
 
@@ -57,37 +61,77 @@ def create_rooms_grid(rooms_data):
     grid = {}
     for room in rooms_data:
         room_id = room['id']
-        grid[room_id] = {
-            {day: {slot: True for slot in SLOTS} for day in range(1, 8)}
-        }
+        grid[room_id] = {day: {slot: True for slot in SLOTS} for day in range(1, 8)}
     return grid
 
 
 
 def main():
-    test = [{"student_id": 1, "weekday": 1, "time_start": "14:00", "time_end": "16:00"}]
 
-    print("Sprawdzam dostępność dla ucznia ID = 1...")
-    print("-" * 40)
+    print("GENEROWANIE PLIKU")
+    
+    path_students = os.path.join('Import', 'students_abavilities.json')
+    path_tutors = os.path.join('Import', 'tutor_avabilities.json')
+    path_rooms = os.path.join('Import', 'rooms.json')
+    path_intentions = os.path.join('Import', 'student_intentions.json')
+    path_csv = os.path.join('Import', 'tutors_subject.csv')
 
-    result = availability_grid(test, "student_id")
-    poniedzialek = result[1][1]
+    try:
+        with open(path_students, 'r', encoding='utf-8') as f: d = json.load(f); students_data = d.get('data', d)
+        with open(path_tutors, 'r', encoding='utf-8') as f: d = json.load(f); tutors_data = d.get('data', d)
+        with open(path_rooms, 'r', encoding='utf-8') as f: d = json.load(f); rooms_data = d.get('data', d)
+        with open(path_intentions, 'r', encoding='utf-8') as f: d = json.load(f); intentions_data = d.get('data', d)
+        tutor_subjects = load_tutor_subject(path_csv)
 
-    for slot, is_ok in poniedzialek.items():
-        if is_ok:
-            print(f"{slot} PASUJE")
-        else:
-            print(f"{slot} NIE PASUJE")
+        print("Generowanie grafików")
+        students_grid = availability_grid(students_data, 'student_id')
+        tutors_grid = availability_grid(tutors_data, 'tutor_id')
+        rooms_grid = create_rooms_grid(rooms_data)
 
-    tutor_test = load_tutor_subject("Import/tutors_subject.csv")
+        print("Łączenie intencji")
+        intentions = []
 
-    if tutor_test:
-        for i, (t_id, sub) in enumerate(tutor_test.items()):
-            if i < 3:
-                print(f"T id = {t_id} uczy {sub}")
-    else:
-        print("Nie wczytani plikow")
+        for intention in intentions_data:
+            try:
+                sub_id = int(intention['subject_id'])
+                stu_id = int(intention['student_id'])
 
+                possible_tutors = []
+                for t_id, sub in tutor_subjects.items():
+                    if sub_id in sub and t_id in tutors_grid:
+                        possible_tutors.append(t_id)
+                
+                pref_loc = intention.get('preferred_location_id')
+                notes = intention.get('notes', "")
+
+                new_intention = {
+                    "id": intention['id'],
+                    "student_id": stu_id,
+                    "subject_id": sub_id,
+                    "weekly_sessions": intention.get('weekly_sessions', 1),
+                    "possible_tutors_ids": possible_tutors,
+                    "preferred_location_id": pref_loc,
+                    "notes": notes
+                }
+                intentions.append(new_intention)
+            except(ValueError, KeyError): continue
+
+        output = {
+            "intentions": intentions,
+            "resources": {
+                "students": students_grid,
+                "tutors": tutors_grid,
+                "rooms": rooms_grid
+            }
+        }
+
+        with open('LESSONSYSTEM_DATA_INPUT.json', 'w', encoding='utf-8') as f:
+            json.dump(output, f, indent=4, ensure_ascii=False)
+        
+        print('-'*40)
+        print("Utworzono plik LESSONSYSTEM_DATA_INPUT.json")
+    except Exception as e:
+        print(f"Błąd {e}")
 
 if __name__ == "__main__":
     main()
